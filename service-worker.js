2b4441a103b0d0b75e1a52d0d0e433a7a51965a6
Matthew Worthington <mworthing@thoughtworks.com>
2022-01-24 20:46:57 +0000

    tweak service worker

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,6 @@
-// Listen for install event, set callback
 self.addEventListener('install', function(event) {
     // Perform some task
 });
 self.addEventListener('fetch', function(event) {
-    event.respondWith(async function() {
-       try{
-         var res = await fetch(event.request);
-         var cache = await caches.open('cache');
-         cache.put(event.request.url, res.clone());
-         return res;
-       }
-       catch(error){
-         return caches.match(event.request);
-        }
-      }());
-  });
+    // Perform some task
+});
