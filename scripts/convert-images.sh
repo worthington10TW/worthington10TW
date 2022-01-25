ac59a184db44964a677419ac64a9e8cdcd0cec01
Matthew Worthington <matt@siliconpanda.com>
2022-01-25 18:57:35 +0000

    Conver image script

--- scripts/convert-images.sh
+++ scripts/convert-images.sh
@@ -5,13 +5,13 @@
 set -e
 
 function install_if_not_exist() {
-    which $1 >/dev/null 2>&1 || brew install $1
+    which $1 >/dev/null 2>&1 || brew install $2
 }
 
 cd "$(dirname "$0")/.."
 
-install_if_not_exist webp
-install_if_not_exist parallel
+install_if_not_exist cwebp webp
+install_if_not_exist parallel parallel
 
 echo "==> Finding jpg, jpeg and png images and converting to webp"
 
